ecdb176126b7776a2aed606f8a8b5e2bd07e14fc
ljbeal <louis.j.beal@gmail.com>
2024-04-10 15:14:06 +0200

    move get_assignment to own module

--- objectcrawler/SlotsCrawler.py
+++ objectcrawler/SlotsCrawler.py
@@ -1,25 +1,7 @@
 """
 Module holding main crawler for __slots__ based objects
 """
-import gc
-
-
-def get_assignment(obj) -> str:
-    """
-    Attempts to extract the assignment location of object `obj` from gc
-
-    :param obj:
-        Object to query
-    :return:
-        assignment location
-    """
-    locations = []
-    for item in gc.get_referrers(obj):
-        if isinstance(item, dict):
-            for k, v in item.items():
-                if v is obj:
-                    locations.append(k)
-    return locations[-1]
+from objectcrawler.get_assignment import get_assignment
 
 
 class SlotsCrawler:
